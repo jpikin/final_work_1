94e78cd406bf8c535b1083000846e2632a78f9ef
Evgeniy Pikin <pikin@bk.ru>
2023-05-30 00:39:26 +0500

    program code added

--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] array = { "Help", "me", "finish", "this", "task", "right", "now" };
+int counter = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length <= 3) counter += 1;
+}
+
+string[] resultArray = new string[counter];
+int resultArrayIndex=0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length <= 3)
+    {
+        resultArray[resultArrayIndex] = array[i];
+        resultArrayIndex += 1;
+    } 
+}
+Console.WriteLine($"[{String.Join(", ", resultArray)}]");
